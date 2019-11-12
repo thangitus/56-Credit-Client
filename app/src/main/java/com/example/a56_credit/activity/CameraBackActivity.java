bd62d4abc5a43809b725ec618bb102525a7156d1
NGUYỄN VĂN THẮNG <1712758@student.hcmus.edu.vn>
2019-11-12 22:51:53 +0700

    Back camera

---
+++ app/src/main/java/com/example/a56_credit/activity/CameraBackActivity.java
@@ -0,0 +1,100 @@
+package com.example.a56_credit.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.camerakit.CameraKitView;
+import com.example.a56_credit.R;
+
+public class CameraBackActivity extends AppCompatActivity {
+   private CameraKitView cameraKitView;
+   ImageView imgFrame;
+   ImageButton buttonTakePic;
+   ImageView buttonClose;
+   View viewRight;
+   TextView textViewTittle;
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_camera_back);
+      mapping();
+      setSizeFrame(imgFrame);
+      buttonTakePic.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+               @Override
+               public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
+                  String base64 = Base64.encodeToString(capturedImage, Base64.DEFAULT);
+               }
+            });
+         }
+      });
+   }
+
+   @Override
+   protected void onStart() {
+      super.onStart();
+      cameraKitView.onStart();
+   }
+
+   @Override
+   protected void onResume() {
+      super.onResume();
+      cameraKitView.onResume();
+   }
+
+   @Override
+   protected void onPause() {
+      cameraKitView.onPause();
+      super.onPause();
+   }
+
+   @Override
+   protected void onStop() {
+      cameraKitView.onStop();
+      super.onStop();
+   }
+
+   @Override
+   public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+      super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+      cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+   }
+
+   private void mapping() {
+      imgFrame = findViewById(R.id.frame);
+      buttonTakePic = findViewById(R.id.buttonTakePic);
+      viewRight = findViewById(R.id.viewRight);
+      textViewTittle = findViewById(R.id.textViewTittle);
+      buttonClose = findViewById(R.id.buttonClose);
+      cameraKitView = findViewById(R.id.camera);
+
+   }
+
+   private float getWidthScreen() {
+      Display display = getWindowManager().getDefaultDisplay();
+      DisplayMetrics outMetrics = new DisplayMetrics();
+      display.getMetrics(outMetrics);
+
+      float dpWidth = outMetrics.widthPixels;
+      return dpWidth;
+   }
+
+   private void setSizeFrame(ImageView imageView) {
+      float width = (float) (getWidthScreen() * 0.8);
+      imageView.getLayoutParams().width = (int) width;
+      imageView.getLayoutParams().height = (int) (width * 1.6);
+      imageView.requestLayout();
+
+   }
+}
--- app/src/main/java/com/example/a56_credit/activity/HomeActivity.java
+++ app/src/main/java/com/example/a56_credit/activity/HomeActivity.java
@@ -6,21 +6,53 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+<<<<<<< Updated upstream
 import android.os.Bundle;
+=======
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+>>>>>>> Stashed changes
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a56_credit.R;
 import com.example.a56_credit.model.PersonalInformation;
 
+<<<<<<< Updated upstream
+=======
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+>>>>>>> Stashed changes
 public class HomeActivity extends AppCompatActivity {
    private static final int REQUEST_CODE = 1;
    ConstraintLayout constraintLayoutCMND, constraintLayoutAddInfo, constraintLayoutInfo;
    TextView tvFullName, tvIdNumber, tvBirthday, tvBuildingNumber, tvWards, tvProvince, tvDistrict;
    TextView tvButtonEdit;
    PersonalInformation personalInformation;
+<<<<<<< Updated upstream
    Intent intentToAddInfo;
+=======
+   Intent intentToAddInfo, intentToCameraBack, intentCameraBack, intentCameraFront;
+   ImageView imgCMND, imgSelfie;
+   Bitmap bitmapCMND, bitmapSelfie;
+   SharedPreferences mPrefs;
+   private Uri mImageUri;
+   String currentPhotoPath;
+>>>>>>> Stashed changes
 
    @SuppressLint("ClickableViewAccessibility")
    @Override
@@ -45,9 +77,43 @@ public class HomeActivity extends AppCompatActivity {
       tvButtonEdit.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View view) {
+<<<<<<< Updated upstream
             intentToAddInfo.putExtra("isEdit",true);
             intentToAddInfo.putExtra("info",personalInformation);
             startActivityForResult(intentToAddInfo,REQUEST_CODE);
+=======
+            intentToAddInfo.putExtra("isEdit", true);
+            intentToAddInfo.putExtra("info", personalInformation);
+            startActivityForResult(intentToAddInfo, REQUEST_CODE_INFO);
+         }
+      });
+      constraintLayoutCMND.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            if (bitmapCMND == null)
+            startActivityForResult(intentToCameraBack, REQUEST_CODE_CAMERA_BACK);
+//               openCameraBack();
+         }
+      });
+      tvReIdenty.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+//            openCameraBack();
+            startActivityForResult(intentToCameraBack, REQUEST_CODE_CAMERA_BACK);
+         }
+      });
+      constraintLayoutSelfie.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            if (bitmapSelfie == null)
+               openCameraFront();
+         }
+      });
+      tvReSelfie.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            openCameraFront();
+>>>>>>> Stashed changes
          }
       });
    }
@@ -81,5 +147,106 @@ public class HomeActivity extends AppCompatActivity {
          constraintLayoutAddInfo.setVisibility(View.VISIBLE);
          tvButtonEdit.setVisibility(View.VISIBLE);
       }
+<<<<<<< Updated upstream
    }
+=======
+      if ((requestCode == REQUEST_CODE_CAMERA_BACK) && (resultCode == Activity.RESULT_OK)) {
+//         Boolean hasPic = data.getBooleanExtra("hasPic", false);
+//         if (hasPic) {
+//            byte[] bytes = data.getByteArrayExtra("image");
+//            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//            imgCMND.setImageBitmap(bmp);
+//         }
+         grabImage(imgCMND, "identity");
+         tvReIdenty.setVisibility(View.VISIBLE);
+      }
+      if ((requestCode == REQUEST_CODE_CAMERA_FRONT) && (resultCode == Activity.RESULT_OK)) {
+         bitmapSelfie = (Bitmap) data.getExtras().get("data");
+         imgSelfie.setImageBitmap(bitmapSelfie);
+         imgSelfie.setVisibility(View.VISIBLE);
+         tvReSelfie.setVisibility(View.VISIBLE);
+         saveBitmap(bitmapSelfie, "selfie");
+      }
+   }
+
+   private void openCameraBack() {
+      intentCameraBack = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+      File photoFile;
+      try {
+         photoFile = createImageFile();
+      } catch (Exception e) {
+         Log.v("Pic", "Can't create file to take picture!");
+         Toast.makeText(this, "Please check SD card! Image shot is impossible!", Toast.LENGTH_SHORT).show();
+         return;
+      }
+      if (photoFile != null) {
+         Uri photoURI = FileProvider.getUriForFile(this,
+                 "com.example.android.fileprovider",
+                 photoFile);
+         intentCameraBack.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+         startActivityForResult(intentCameraBack, REQUEST_CODE_CAMERA_BACK);
+      }
+   }
+//      startActivityForResult(intentCameraBack, REQUEST_CODE_CAMERA_BACK);
+
+   private void openCameraFront() {
+      intentCameraFront = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+      intentCameraFront.putExtra("android.intent.extras.CAMERA_FACING", 1);
+      startActivityForResult(intentCameraFront, REQUEST_CODE_CAMERA_FRONT);
+   }
+
+   private void saveBitmap(Bitmap bitmap, String key) {
+      SharedPreferences.Editor editor = mPrefs.edit();
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+      byte[] bytes = baos.toByteArray();
+      String encoded = Base64.encodeToString(bytes, Base64.DEFAULT);
+      if (key.equals("identity"))
+         personalInformation.setIdentity(encoded);
+      else
+         personalInformation.setSelfie(encoded);
+      editor.putString(key, encoded);
+      editor.apply();
+   }
+
+   private Bitmap loadBitmap(String key) {
+      String encoded = mPrefs.getString(key, null);
+      if (encoded == null)
+         return null;
+      byte[] imageAsBytes = Base64.decode(encoded.getBytes(), Base64.DEFAULT);
+      Bitmap bm = BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length);
+      return bm;
+   }
+
+   private File createImageFile() throws IOException {
+      // Create an image file name
+      String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+      String imageFileName = "JPEG_" + timeStamp + "_";
+      File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+      File image = File.createTempFile(
+              imageFileName,  /* prefix */
+              ".JPG",         /* suffix */
+              storageDir      /* directory */
+      );
+
+      // Save a file: path for use with ACTION_VIEW intents
+      currentPhotoPath = image.getAbsolutePath();
+      return image;
+   }
+
+   public void grabImage(ImageView imageView, String key) {
+      this.getContentResolver().notifyChange(mImageUri, null);
+      ContentResolver cr = this.getContentResolver();
+      Bitmap bitmap;
+      try {
+         bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, mImageUri);
+         saveBitmap(bitmap, key);
+         imageView.setImageBitmap(bitmap);
+      } catch (Exception e) {
+         Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+         Log.d("Pic", "Failed to load", e);
+      }
+      imageView.setVisibility(View.VISIBLE);
+   }
+>>>>>>> Stashed changes
 }
