3c227e61e43d0a2e75b38d7eac6b9247b3f1a079
thangitus <thang.itus@gmail.com>
2019-11-15 09:44:07 +0700

    FotoApparat
    Sử dụng FotoApparat thay cho CameraX

--- app/src/main/java/com/example/a56_credit/activity/CameraBackActivity.java
+++ app/src/main/java/com/example/a56_credit/activity/CameraBackActivity.java
@@ -1,45 +1,51 @@
 package com.example.a56_credit.activity;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraX;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureConfig;
-import androidx.camera.core.Preview;
-import androidx.camera.core.PreviewConfig;
-import androidx.camera.core.impl.utils.executor.CameraXExecutors;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Base64;
 import android.util.DisplayMetrics;
-import android.util.Size;
 import android.view.Display;
-import android.view.TextureView;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.a56_credit.R;
 
-import java.io.ByteArrayOutputStream;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.error.CameraErrorListener;
+import io.fotoapparat.exception.camera.CameraException;
+import io.fotoapparat.parameter.ScaleType;
+import io.fotoapparat.result.PhotoResult;
+import io.fotoapparat.result.WhenDoneListener;
+import io.fotoapparat.view.CameraView;
+import kotlin.Unit;
+
+import static io.fotoapparat.log.LoggersKt.fileLogger;
+import static io.fotoapparat.log.LoggersKt.logcat;
+import static io.fotoapparat.log.LoggersKt.loggers;
+import static io.fotoapparat.selector.AspectRatioSelectorsKt.standardRatio;
+import static io.fotoapparat.selector.LensPositionSelectorsKt.back;
+import static io.fotoapparat.selector.ResolutionSelectorsKt.highestResolution;
 
 public class CameraBackActivity extends AppCompatActivity {
    ImageView imgFrame;
    ImageButton buttonTakePic;
    ImageView buttonClose;
-   View viewRight, viewLeft, viewTop, viewBottom;
+   View viewRight;
    TextView textViewTittle;
-   TextureView textureView;
+   CameraView cameraView;
+   Fotoapparat fotoapparat;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -48,44 +54,25 @@ public class CameraBackActivity extends AppCompatActivity {
       final Intent intent = getIntent();
       mapping();
       setSizeFrame(imgFrame);
-      CameraX.unbindAll();
-      Preview preview = createPreview();
-      ImageCapture imageCapture = createImageCapture();
-      preview.setOnPreviewOutputUpdateListener(
-              new Preview.OnPreviewOutputUpdateListener() {
-                 @Override
-                 public void onUpdated(Preview.PreviewOutput previewOutput) {
-                    // The output data-handling is configured in a listener.
-                    textureView.setSurfaceTexture(previewOutput.getSurfaceTexture());
-                    // Your custom code here.
-                 }
-              });
-
-// The use case is bound to an Android Lifecycle with the following code.
-      CameraX.bindToLifecycle(this, imageCapture, preview);
-
       buttonTakePic.setOnClickListener(new View.OnClickListener() {
-         @SuppressLint("RestrictedApi")
          @Override
          public void onClick(View view) {
-            File file = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".jpg");
-            imageCapture.takePicture(file, CameraXExecutors.mainThreadExecutor(), new ImageCapture.OnImageSavedListener() {
+            final PhotoResult photoResult = fotoapparat.takePicture();
+            Date date = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+            final File file = new File(getExternalFilesDir("Photos"), dateFormat.format(date) + ".JPG");
+            photoResult.saveToFile(file).whenDone(new WhenDoneListener<Unit>() {
                @Override
-               public void onImageSaved(@NonNull File file) {
+               public void whenDone(@Nullable Unit unit) {
                   intent.putExtra("hasPhoto", true);
                   intent.putExtra("photo", file.getAbsolutePath());
                   setResult(Activity.RESULT_OK, intent);
                   finish();
                }
-
-               @Override
-               public void onError(@NonNull ImageCapture.ImageCaptureError imageCaptureError, @NonNull String message, @Nullable Throwable cause) {
-
-               }
             });
+
          }
       });
-
       buttonClose.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View view) {
@@ -94,44 +81,8 @@ public class CameraBackActivity extends AppCompatActivity {
             finish();
          }
       });
+      fotoapparat = createFotoapparat();
    }
-
-   private ImageCapture createImageCapture() {
-      ImageCaptureConfig imageCaptureConfig =
-              new ImageCaptureConfig.Builder()
-                      .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation())
-                      .setLensFacing(CameraX.LensFacing.BACK)
-                      .build();
-
-      ImageCapture imageCapture = new ImageCapture(imageCaptureConfig);
-      return imageCapture;
-   }
-
-   private Preview createPreview() {
-      PreviewConfig previewConfig = new PreviewConfig.Builder()
-              .setLensFacing(CameraX.LensFacing.BACK)
-              .build();
-      Preview preview = new Preview(previewConfig);
-      return preview;
-   }
-
-   private byte[] cropIMG(byte[] capturedImage) {
-      Bitmap bitmap = BitmapFactory.decodeByteArray(capturedImage, 0, capturedImage.length);
-      float widthScreen, heightScreen, heightTop, heightBottom, widthLeft, widthRight;
-      widthScreen = getWidthScreen();
-      heightScreen = getHeightScreen();
-      heightTop = viewTop.getHeight();
-      heightBottom = viewBottom.getHeight();
-      widthLeft = viewLeft.getWidth();
-      widthRight = viewRight.getHeight();
-      Bitmap res = Bitmap.createBitmap(bitmap, 0, 0, (int) (widthScreen - widthRight), (int) (heightScreen - heightBottom));
-      res = Bitmap.createBitmap(res, (int) imgFrame.getX(), (int) imgFrame.getY(), res.getWidth() - (int) widthLeft, res.getHeight() - (int) heightTop);
-      ByteArrayOutputStream stream = new ByteArrayOutputStream();
-      res.compress(Bitmap.CompressFormat.PNG, 100, stream);
-      byte[] byteArray = stream.toByteArray();
-      return byteArray;
-   }
-
 
    private void mapping() {
       imgFrame = findViewById(R.id.frame);
@@ -139,26 +90,15 @@ public class CameraBackActivity extends AppCompatActivity {
       viewRight = findViewById(R.id.viewRight);
       textViewTittle = findViewById(R.id.textViewTittle);
       buttonClose = findViewById(R.id.buttonClose);
-      viewLeft = findViewById(R.id.viewLeft);
-      viewTop = findViewById(R.id.viewTop);
-      viewBottom = findViewById(R.id.viewBottom);
-      textureView = findViewById(R.id.view_finder);
+      cameraView = findViewById(R.id.camera_view);
    }
 
    private float getWidthScreen() {
       Display display = getWindowManager().getDefaultDisplay();
       DisplayMetrics outMetrics = new DisplayMetrics();
       display.getMetrics(outMetrics);
-      float width = outMetrics.widthPixels;
-      return width;
-   }
-
-   private float getHeightScreen() {
-      Display display = getWindowManager().getDefaultDisplay();
-      DisplayMetrics outMetrics = new DisplayMetrics();
-      display.getMetrics(outMetrics);
-      float height = outMetrics.widthPixels;
-      return height;
+      float dpWidth = outMetrics.widthPixels;
+      return dpWidth;
    }
 
    private void setSizeFrame(ImageView imageView) {
@@ -167,5 +107,39 @@ public class CameraBackActivity extends AppCompatActivity {
       imageView.getLayoutParams().height = (int) (width * 1.6);
       imageView.requestLayout();
 
+   }
+
+   @Override
+   protected void onStart() {
+      super.onStart();
+      fotoapparat.start();
+   }
+
+   @Override
+   protected void onPause() {
+      super.onPause();
+      fotoapparat.stop();
+   }
+
+   private Fotoapparat createFotoapparat() {
+      return Fotoapparat
+              .with(this)
+              .into(cameraView)
+              .previewScaleType(ScaleType.CenterCrop)
+              .lensPosition(back())
+              .logger(loggers(
+                      logcat(),
+                      fileLogger(this)
+              ))
+              .cameraErrorCallback(new CameraErrorListener() {
+                 @Override
+                 public void onError(@NotNull CameraException e) {
+                    Toast.makeText(CameraBackActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+                 }
+              })
+              .photoResolution(standardRatio(
+                      highestResolution()
+              ))
+              .build();
    }
 }
--- app/src/main/java/com/example/a56_credit/activity/CameraFrontActivity.java
+++ app/src/main/java/com/example/a56_credit/activity/CameraFrontActivity.java
@@ -1,34 +1,45 @@
 package com.example.a56_credit.activity;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraX;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureConfig;
-import androidx.camera.core.Preview;
-import androidx.camera.core.PreviewConfig;
-import androidx.camera.core.impl.utils.executor.CameraXExecutors;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
-import android.view.Surface;
-import android.view.TextureView;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.a56_credit.R;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.error.CameraErrorListener;
+import io.fotoapparat.exception.camera.CameraException;
+import io.fotoapparat.parameter.ScaleType;
+import io.fotoapparat.result.PhotoResult;
+import io.fotoapparat.result.WhenDoneListener;
+import io.fotoapparat.view.CameraView;
+import kotlin.Unit;
+
+import static io.fotoapparat.log.LoggersKt.fileLogger;
+import static io.fotoapparat.log.LoggersKt.logcat;
+import static io.fotoapparat.log.LoggersKt.loggers;
+import static io.fotoapparat.selector.AspectRatioSelectorsKt.standardRatio;
+import static io.fotoapparat.selector.LensPositionSelectorsKt.front;
+import static io.fotoapparat.selector.ResolutionSelectorsKt.highestResolution;
 
 public class CameraFrontActivity extends AppCompatActivity {
    ImageButton buttonTakePhoto;
    ImageView buttonClose;
-   TextureView textureView;
+   CameraView cameraView;
+   Fotoapparat fotoapparat;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -37,29 +48,6 @@ public class CameraFrontActivity extends AppCompatActivity {
       final Intent intent;
       intent = getIntent();
       mapping();
-      PreviewConfig config = new PreviewConfig.Builder().setLensFacing(CameraX.LensFacing.FRONT).build();
-      Preview preview = new Preview(config);
-      CameraX.unbindAll();
-      ImageCaptureConfig config1 =
-              new ImageCaptureConfig.Builder()
-                      .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation())
-                      .setLensFacing(CameraX.LensFacing.FRONT)
-                      .build();
-
-      ImageCapture imageCapture = new ImageCapture(config1);
-
-      preview.setOnPreviewOutputUpdateListener(
-              new Preview.OnPreviewOutputUpdateListener() {
-                 @Override
-                 public void onUpdated(Preview.PreviewOutput previewOutput) {
-                    // The output data-handling is configured in a listener.
-                    textureView.setSurfaceTexture(previewOutput.getSurfaceTexture());
-                    // Your custom code here.
-                 }
-              });
-
-// The use case is bound to an Android Lifecycle with the following code.
-      CameraX.bindToLifecycle(this, imageCapture, preview);
 
       buttonClose.setOnClickListener(new View.OnClickListener() {
          @Override
@@ -70,38 +58,64 @@ public class CameraFrontActivity extends AppCompatActivity {
          }
       });
       buttonTakePhoto.setOnClickListener(new View.OnClickListener() {
-         @SuppressLint("RestrictedApi")
          @Override
          public void onClick(View view) {
-            File file = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".jpg");
-            imageCapture.takePicture(file, CameraXExecutors.mainThreadExecutor(), new ImageCapture.OnImageSavedListener() {
+            final PhotoResult photoResult = fotoapparat.takePicture();
+            Date date = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+            final File file = new File(getExternalFilesDir("Photos"), dateFormat.format(date) + ".JPG");
+            photoResult.saveToFile(file).whenDone(new WhenDoneListener<Unit>() {
                @Override
-               public void onImageSaved(@NonNull File file) {
+               public void whenDone(@Nullable Unit unit) {
                   intent.putExtra("hasPhoto", true);
                   intent.putExtra("photo", file.getAbsolutePath());
                   setResult(Activity.RESULT_OK, intent);
                   finish();
                }
-               @Override
-               public void onError(@NonNull ImageCapture.ImageCaptureError imageCaptureError, @NonNull String message, @Nullable Throwable cause) {
-
-               }
             });
          }
       });
+      fotoapparat = createFotoapparat();
    }
 
-   private void sendToHomeActivity(Intent intent, byte[] bytes) {
-      intent.putExtra("hasPhoto", true);
-      intent.putExtra("photo", bytes);
-      setResult(Activity.RESULT_OK, intent);
-      finish();
-   }
 
    private void mapping() {
       buttonTakePhoto = findViewById(R.id.buttonTakePicFront);
       buttonClose = findViewById(R.id.buttonCloseFront);
-      textureView = findViewById(R.id.view_finder);
+      cameraView = findViewById(R.id.camera_front);
    }
 
+   private Fotoapparat createFotoapparat() {
+      return Fotoapparat
+              .with(this)
+              .into(cameraView)
+              .previewScaleType(ScaleType.CenterCrop)
+              .lensPosition(front())
+              .logger(loggers(
+                      logcat(),
+                      fileLogger(this)
+              ))
+              .cameraErrorCallback(new CameraErrorListener() {
+                 @Override
+                 public void onError(@NotNull CameraException e) {
+                    Toast.makeText(CameraFrontActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+                 }
+              })
+              .photoResolution(standardRatio(
+                      highestResolution()
+              ))
+              .build();
+   }
+
+   @Override
+   protected void onStart() {
+      super.onStart();
+      fotoapparat.start();
+   }
+
+   @Override
+   protected void onPause() {
+      super.onPause();
+      fotoapparat.stop();
+   }
 }
